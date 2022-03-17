f148b9c184e25c77d39c4fa18335d99bb787c9ff
Pallavi-Mali <98145856+Pallavi-Mali@users.noreply.github.com>
2022-03-17 22:30:55 +0530

    Add files via upload

---
+++ assign2_2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+int pid1, pid2;
+
+pid1 = fork();
+
+if(pid1==0)
+	{
+	 printf("Child 1 is started: %d\n", getpid());
+	 execl("/bin/ls", "ls", "-lh", 0);	
+	}
+else
+	{
+	 pid2 = fork();
+	 if(pid2==0)
+		{
+	 	 printf("Child 2 started: %d\n", getpid());
+	 	 printf("Parent PID of Child 2: %d\n", getppid());
+	 	 //execl("/home/ameysystem/static_main", "./static_main", 0);			
+		}
+	 else
+		{
+ 	 	 //sleep(3);
+	 	 printf("Parent PID\n");
+	  	 printf("Parent PID: %d\n",getpid());	
+		}
+	}
+
+while(1);
+return(0);
+}
---
+++ assign2_3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+ execl("/home/Downloads/assign_ps", "./assign_ps", 0);
+
+ return 0;
+}
